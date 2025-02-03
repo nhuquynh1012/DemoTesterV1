138b362ec7998716e210ea246ffa30f71fd81629
nhuquynh <qp7139@gmail.com>
2025-02-03 21:26:41 +0700

    update file

---
+++ test/src/main/java/com/pnnq/test/Test_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pnnq.test;
+
+/**
+ *
+ * @author Như Quỳnh
+ */
+public class Test_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
