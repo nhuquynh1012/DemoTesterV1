e49ffca694eeb54cc678236637be5b3cf8185f52
nhuquynh <qp7139@gmail.com>
2025-02-03 20:47:51 +0700

    them thu muc moi

---
+++ mavenproject1/src/main/java/com/pnnq/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pnnq.mavenproject1;
+
+/**
+ *
+ * @author Như Quỳnh
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("test");
+    }
+}
