8f64b8ff0f99f797c6ae5903bc76ba186ca2dedd
nhuquynh <qp7139@gmail.com>
2025-02-03 20:35:02 +0700

    tao thu muc java

---
+++ BMIApp/src/main/java/com/pnnq/bmiapp/BMIApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pnnq.bmiapp;
+
+/**
+ *
+ * @author Như Quỳnh
+ */
+public class BMIApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
