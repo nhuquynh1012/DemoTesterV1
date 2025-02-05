6558743ecdde91ab74a9d4de52ea178944c04b76
nhuquynh <qp7139@gmail.com>
2025-02-05 22:57:43 +0700

    update

--- BMIApp/src/main/java/com/pnnq/bmiapp/PrimaryController.java
+++ BMIApp/src/main/java/com/pnnq/bmiapp/PrimaryController.java
@@ -7,6 +7,9 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        int a= 0;
+        if(a<0){
+                App.setRoot("secondary");
+        }
     }
 }
