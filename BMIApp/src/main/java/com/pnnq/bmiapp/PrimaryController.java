9bf6d0a2aebc656ecbe625b890c88623f31aff7d
nhuquynh <qp7139@gmail.com>
2025-02-06 23:19:51 +0700

    update b2

--- BMIApp/src/main/java/com/pnnq/bmiapp/PrimaryController.java
+++ BMIApp/src/main/java/com/pnnq/bmiapp/PrimaryController.java
@@ -1,21 +1,34 @@
 package com.pnnq.bmiapp;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-<<<<<<< HEAD
-        int a= 0;
-        if(a<0){
-                App.setRoot("secondary");
-=======
-        int a=0;
-        if(a<0&&a%2==0){
-        App.setRoot("secondary");
->>>>>>> main
+    @FXML private TextField txtHeight;
+    @FXML private TextField txtWeight;
+    @FXML private Label lblKetQua;
+    
+    public void tinhCanNang(ActionEvent Evt){
+        double h =Double.parseDouble(this.txtHeight.getText());
+        double w = Double.parseDouble(this.txtWeight.getText());
+        double bmi = w/Math.pow(h, 2);
+        String result;
+        if(bmi<18.5){
+            result = "Gầy";
+            this.lblKetQua.setTextFill(Color.RED);            
         }
+        else if(bmi<25){
+            result = "Bình Thường";
+            this.lblKetQua.setTextFill(Color.GREEN);
+        }
+        else{
+            result= "Béo Phì";
+            this.lblKetQua.setTextFill(Color.YELLOW);
+        }
+        
+        this.lblKetQua.setText(result);
     }
 }
